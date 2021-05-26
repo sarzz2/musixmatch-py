867b4c0a03fd1c691ce4206ca22016f6723b4e6d
sarzz2 <sarthaksingh2001@gmail.com>
2021-05-26 23:17:43 +0530

    docs test

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(''))
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
